81f86974ecdd19d80c983cf2856a2cad2df3cdd0
AdolfoMontes <82065801+AdolfoMontes@users.noreply.github.com>
2021-07-08 19:42:41 -0400

    Ultimos cambios

--- Proyecto_xpress/aplicacion_xpress/models.py
+++ Proyecto_xpress/aplicacion_xpress/models.py
@@ -6,17 +6,17 @@ from .choices import tipo_cliente, sexo, tipo_tarjeta, tipo_doc, tipo_vehiculo
 
 
 class Tarjeta(models.Model):
-    rut =  models.CharField(max_length=17, null=False, blank=False, unique=True, verbose_name='RUT')
+    rut =  models.CharField(max_length=17, null=False, blank=False, verbose_name='RUT')
     pri_nom = models.CharField(max_length=30, null=False, blank=False,verbose_name='Primer Nombre')
-    seg_nom = models.CharField(max_length=30, verbose_name='Segundo Nombre')
+    seg_nom = models.CharField(max_length=30,  null=False, blank=False,verbose_name='Segundo Nombre')
     pri_ap = models.CharField(max_length=30, null=False, blank=False, verbose_name='Apelldo Paterno')
-    seg_ap = models.CharField(max_length=30, verbose_name='Apellido Materno')
+    seg_ap = models.CharField(max_length=30,  null=False, blank=False,verbose_name='Apellido Materno')
     direccion = models.CharField(max_length=50,  null=False, blank=False, verbose_name='Dirección')
     tipo_cliente = models.CharField(max_length=1, choices=tipo_cliente, default ='1', verbose_name='Tipo de Cliente')
     sexo = models.CharField(max_length=1, choices=sexo, default='F', verbose_name='Sexo')
     numero_tarjeta =  models.CharField(max_length=17, null=False, blank=False, unique=True, verbose_name='Número de tarjeta')
     tipo_tarjeta = models.CharField(max_length=1, null=False, blank=False, choices=tipo_tarjeta, default = '1', verbose_name='Tipo de tarjeta')
-    tipo_doc = models.CharField(max_length=1, null=True, blank=True, choices=tipo_doc,  verbose_name='Tipo de documento ')
+    tipo_doc = models.CharField(max_length=1, null=False, blank=False, choices=tipo_doc,  verbose_name='Tipo de documento ')
     saldo = models.IntegerField(default=0, null=False, blank=False)
     patente = models.CharField(max_length=16, null=True, blank=False, unique=True, verbose_name='Patente')
     tipo_vehiculo = models.CharField(max_length=30, choices=tipo_vehiculo, default='1', verbose_name='Tipo de vehiculo')
